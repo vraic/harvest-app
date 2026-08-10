152e773d69da049cf4f7e73d8dec21ca064dfde1
André <288215058+vraic@users.noreply.github.com>
2026-08-10 20:35:31 +0100

    #149 controller, model, policy updates (#150)
    * #149 controller, model, policy, view updates

--- app/controllers/data_subject_requests_controller.rb
+++ app/controllers/data_subject_requests_controller.rb
@@ -30,6 +30,7 @@ class DataSubjectRequestsController < ApplicationController
 
     @data_subject_request = DataSubjectRequest.new(permitted_params)
     @data_subject_request.requester = Current.user
+    @data_subject_request.account = Current.account unless Current.user.admin?
     @data_subject_request.account ||= Current.account
     authorize @data_subject_request
 
@@ -74,17 +75,17 @@ class DataSubjectRequestsController < ApplicationController
   end
 
   def set_form_options
-    @available_accounts = if Current.user.admin?
-      Account.order(:name)
-    else
-      [ Current.account ].compact
-    end
-
     account = @data_subject_request&.account || Current.account
     @available_subject_users = if account
-      User.joins(:account_users).where(account_users: { account_id: account.id }).distinct.order(:name)
+      AccountUser.active
+        .where(account_id: account.id)
+        .includes(:user)
+        .map(&:user)
+        .compact
+        .uniq
+        .sort_by { |user| user.name.to_s }
     else
-      [ Current.user ].compact
+      []
     end
   end
 

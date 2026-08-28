6e125f7837fdc1a96601f3a2a458635e8dfa3ed0
vraic <288215058+vraic@users.noreply.github.com>
2026-08-28 17:09:04 +0100

    update homepage test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -6,9 +6,6 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
     assert_select "h1", "Harvest exists to inspire new ways of thinking and being, so that everyone can live life to the fullest."
-    assert_select "nav[aria-label='Homepage'] a[href='#invitation']", text: "An invitation"
-    assert_select "nav[aria-label='Homepage'] a[href='#look']", text: "Look inside"
-    assert_select "nav[aria-label='Homepage'] a[href='#questions']", text: "Hard questions"
     assert_select "a[href=?]", new_session_path, text: "Login", minimum: 1
     assert_select "a[href=?]", new_session_path, text: "Get started"
   end
